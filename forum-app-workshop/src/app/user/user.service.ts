3dad878029bc5c3243eb4b95d2cd105ea132213b
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-07-13 16:08:04 +0300

    add user to localStorage through the user service

--- forum-app-workshop/src/app/user/user.service.ts
+++ forum-app-workshop/src/app/user/user.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { UserForAuth } from '../types/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  user: UserForAuth | undefined;
+  USER_KEY = '[user]';
 
-  constructor() { }
+  get isAuth(): boolean {
+    return !!this.user;
+  }
+
+  constructor() {
+    try {
+      const lsUser = localStorage.getItem(this.USER_KEY) || '';
+      this.user = JSON.parse(lsUser);
+    } catch (error) {
+      this.user = undefined;
+    }
+  }
+  login() {
+    this.user = {
+      firstName: 'Peter',
+      email: 'peter@gmail.com',
+      phoneNumber: '123-456',
+      password: '1234',
+    };
+
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.removeItem(this.USER_KEY);
+  }
+  register() {}
 }
