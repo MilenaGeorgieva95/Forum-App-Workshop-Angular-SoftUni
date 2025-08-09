190e9a516afb9f27130303a3ba5de3a313d03140
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-08-09 11:28:13 +0300

    add reactive form controls to register form

--- forum-app-workshop/src/app/user/login/login.component.ts
+++ forum-app-workshop/src/app/user/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user.service';
 import { NgForm } from '@angular/forms';
+import { EMAIL_DOMAINS } from 'src/app/constants';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +9,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  domains=EMAIL_DOMAINS;
   constructor(private userService: UserService) {}
 
   login(form:NgForm) {
