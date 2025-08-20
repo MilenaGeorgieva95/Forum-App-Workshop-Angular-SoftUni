b967d2c5b27723622f6a7a0f7944aaf1b7fa9c4c
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-08-20 23:44:54 +0300

    change url through and add cookies app interceptor and

--- forum-app-workshop/src/app/app.interceptor.ts
+++ forum-app-workshop/src/app/app.interceptor.ts
@@ -7,6 +7,9 @@ import {
   HTTP_INTERCEPTORS
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+const {apiUrl}=environment
 
 @Injectable()
 class AppInterceptor implements HttpInterceptor {
@@ -14,6 +17,13 @@ class AppInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if(request.url.startsWith('/api')){
+      request=request.clone({
+        url:request.url.replace('/api', apiUrl),
+        withCredentials:true
+      })
+    }
+    
     return next.handle(request);
   }
 }
