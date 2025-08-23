8a44876e87c091542238ec532c07edcc91628944
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-08-23 12:38:09 +0300

    add slice pipe

--- forum-app-workshop/src/app/core/global-error/global-error.component.ts
+++ forum-app-workshop/src/app/core/global-error/global-error.component.ts
@@ -4,16 +4,15 @@ import { GlobalErrorService } from './global-error.service';
 @Component({
   selector: 'app-global-error',
   templateUrl: './global-error.component.html',
-  styleUrls: ['./global-error.component.css']
+  styleUrls: ['./global-error.component.css'],
 })
-export class GlobalErrorComponent implements OnInit{
-errorMsg:Error | null=null
-constructor(private globalErrorService: GlobalErrorService){}
+export class GlobalErrorComponent implements OnInit {
+  errorMsg: Error | null = null;
+  constructor(private globalErrorService: GlobalErrorService) {}
 
-ngOnInit(): void {
-  this.globalErrorService.apiError$.subscribe(err=>{
-    this.errorMsg=err
-    
-  })
-}
+  ngOnInit(): void {
+    this.globalErrorService.apiError$.subscribe((err: Error | null) => {
+      this.errorMsg = err;
+    });
+  }
 }
--- forum-app-workshop/src/app/shared/shared.module.ts
+++ forum-app-workshop/src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { LoaderComponent } from './loader/loader.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { RouterModule } from '@angular/router';
 import { EmailDirective } from './validators/email.directive';
+import { SlicePipe } from './pipes/slice.pipe';
 
 @NgModule({
-  declarations: [LoaderComponent, WelcomeComponent, EmailDirective],
+  declarations: [LoaderComponent, WelcomeComponent, EmailDirective, SlicePipe],
   imports: [CommonModule, RouterModule],
-  exports: [LoaderComponent, WelcomeComponent, EmailDirective],
+  exports: [LoaderComponent, WelcomeComponent, EmailDirective, SlicePipe],
 })
 export class SharedModule {}
